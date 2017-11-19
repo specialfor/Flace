58810bc46f16d18ce72001103db98b8f2cc0b83f
Volodymyr Hryhoriev <v.hryhoriev@tecsynt.com>
2017-11-19 10:07:13 +0200

    Add 'Tag' model. Update 'Place' model.

---
+++ Chuh-chuh/Library/Models/Tag.swift
@@ -0,0 +1,29 @@
+//
+//  Tag.swift
+//  Chuh-chuh
+//
+//  Created by Volodymyr Hryhoriev on 11/19/17.
+//  Copyright © 2017 Nobles Team. All rights reserved.
+//
+
+import Foundation
+
+enum Tag: Int, Codable {
+    case romantic
+    case relax
+    case sport
+    case extreme
+    case art
+    
+    var title: String {
+        return Tag.titles[self.rawValue]
+    }
+    
+    static let titles = [
+        "Romantic",
+        "Relax",
+        "Sport",
+        "Extreme",
+        "Art"
+    ]
+}
